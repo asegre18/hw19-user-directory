c42adf6cd87053ffe9bf98110210582a73e41f74
Andrew Segre <asegre18@cmc.edu>
2020-12-10 16:40:12 -0800

    deployment changes

---
+++ build/precache-manifest.3d709b3d68863f81ab9aafec58552e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1f239210955b104b29ffe75f49a792",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14c7594c1807102eb081",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ff5c8e712c2a8c5272b2",
+    "url": "/static/css/main.16f2466d.chunk.css"
+  },
+  {
+    "revision": "14c7594c1807102eb081",
+    "url": "/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "ff5c8e712c2a8c5272b2",
+    "url": "/static/js/main.48c45ef3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
